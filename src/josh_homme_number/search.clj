535723bbf14101202f99ece2a763b014a39c4cd0
Dane Johnson <dane_johnson@ymail.com>
2017-05-20 15:46:18 -0500

    Second implementation idea

--- src/josh_homme_number/search.clj
+++ src/josh_homme_number/search.clj
@@ -2,15 +2,23 @@
   (:require [josh-homme-number.scraper :refer :all])
   (:gen-class))
 
-(def next-order
-  "Gets the next order of artists after a given artist"
-  (memoize
-   ;; Artists collaborate with the same people a lot, no need to
-   ;; Be repeating http requests
-   (fn         
-     [url]
-     (let [bands (get-bands (fetch-url url))]
-       (map #(hash-map (first %) (get-artists (fetch-url (second %)))) bands)))))
+(defrecord Node [artist links])
+(defrecord Link [band nodes])
+
+(def root ())
+
+(def visited (atom #{}))
+
+(defn search
+  "Searches for the specified artist and returns the shortest path to
+  her or nil if she is not found"
+  [node artist max-depth]
+  (cond
+    (= (second node) artist) (list) ;; We've found her, back up the line!
+    (or (zero? (dec max-depth)) (@visited artist)) nil ;; No dice
+    :else (do
+            (swap! visited conj artist)
+            (let))))
 
 ;; REPL helpers
 (def joshs-url josh-homme-number.core/*base-url*)
