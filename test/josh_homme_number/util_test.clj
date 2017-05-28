f2088848aa9abf820ae82091ac0cb853c70f6033
Dane Johnson <dane_johnson@ymail.com>
2017-05-27 21:21:59 -0500

    Formatted line seperator in tests for multiplatform support

--- test/josh_homme_number/util_test.clj
+++ test/josh_homme_number/util_test.clj
@@ -2,7 +2,7 @@
   (:require [expectations :refer :all]
             [josh-homme-number.util :refer :all]))
 
-(expect "Arthur Dent was in The Resturant at the End of the Universe with Ford Prefect who was in Life, the Universe and Everything with Hotblack Desatio.\n"
+(expect (format "Arthur Dent was in The Resturant at the End of the Universe with Ford Prefect who was in Life, the Universe and Everything with Hotblack Desatio%n")
         (with-out-str
           (print-path
            '("Arthur Dent" "The Resturant at the End of the Universe" "Ford Prefect" "Life, the Universe and Everything" "Hotblack Desatio"))))
